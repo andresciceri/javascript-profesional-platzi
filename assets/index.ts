75506b03a9dcc263b2712af2566660df0494c882
Andres Escobar Ciceri <andresciceri@gmail.com>
2020-07-24 21:31:27 -0500

    feat: :sparkles: Singleton, Decorators & plugin Ads

--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,15 @@
 import MediaPlayer from "./MediaPlayer";
 import AutoPlay from "./plugins/AutoPlay";
 import AutoPause from "./plugins/AutoPause";
+import AdsPlugin from "./plugins/Ads";
+
 const video = document.querySelector("video");
 const buttonPlay = document.getElementById("play");
 const buttonUnmute = document.getElementById("unmute");
 
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay(), new AutoPause()],
+  plugins: [new AutoPlay(), new AutoPause(), new AdsPlugin()],
 });
 buttonPlay.onclick = () =>
   player.media.paused ? player.play() : player.pause();
